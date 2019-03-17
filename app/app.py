84d1e45ffebce92cf38f032803131495592b5fbe
Justin Frevert <jgaingringer@gmail.com>
2019-03-17 03:25:38 -0400

    Fixing up api

--- app/app.py
+++ app/app.py
@@ -1,23 +1,27 @@
-from flask import Flask, Response
+from flask import Flask, Response, request, jsonify, stream_with_context
 from pymongo import MongoClient
+import json
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://db:27017/')
 db = client.test_database
 collection = db.test_collection
 posts = db.posts
-# post_id = posts.insert_one(post).inserted_id
-
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello():
-    return Response("Hi from your Flask app running in your Docker container!")
+    return Response(db.collection_names())
 
-@app.route("/user")
-def hello():
-    return Response("Hi from your Flask app running in your Docker container!")
+@app.route("/new-user", methods=['POST'])
+def user_response():
+    req_data = request.get_json()
+    name = request.json['name']
+    email = request.json['email']
+    post_id = posts.insert_one({'name': name, 'email': email}).inserted_id
+    new_post = posts.find_one({'_id': post_id })
+    output = {'name' : new_post['name'], 'email' : new_post['email']}
+    return jsonify({'result' : output})
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=80, debug=True)
