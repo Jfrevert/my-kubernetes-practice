c4a51efc2386b70c0ebf16f8d26930449215b12a
Justin Frevert <jgaingringer@gmail.com>
2019-03-31 13:31:37 -0400

    small fix

--- frontend/serve.js
+++ frontend/serve.js
@@ -7,28 +7,12 @@ app.use(cors());
 app.options('*', cors());
 
 var allowCrossDomain = function(req, res, next) {
-  console.log('AH?')
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin");
   res.header("Access-Control-Allow-Origin", "*");
   next();
 }
 
 app.use(allowCrossDomain);
-
-// module.exports = function (req, res, next) {
-//   // CORS headers
-//   res.header("Access-Control-Allow-Origin", "YOUR_URL"); // restrict it to the required domain
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//   // Set custom headers for CORS
-//   res.header("Access-Control-Allow-Headers", "Content-type,Accept,X-Custom-Header");
-
-//   if (req.method === "OPTIONS") {
-//       return res.status(200).end();
-//   }
-
-//   return next();
-// };
-
 
 
 app.use(express.static(path.join(__dirname, 'build')));
