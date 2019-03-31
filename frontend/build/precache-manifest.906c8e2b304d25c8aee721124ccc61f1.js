cf91c77881d44ef21993204eddf16bf7ca5f3189
Justin Frevert <jgaingringer@gmail.com>
2019-03-31 14:02:08 -0400

    Added build directory, Docker can't puull in the necessary files to build frontend inside the container

---
+++ frontend/build/precache-manifest.906c8e2b304d25c8aee721124ccc61f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "22727972d6370571d6f3",
+    "url": "/static/js/main.0d97bdc2.chunk.js"
+  },
+  {
+    "revision": "f1cdee26082c0f1e2cfa",
+    "url": "/static/js/2.9f1cf8c1.chunk.js"
+  },
+  {
+    "revision": "22727972d6370571d6f3",
+    "url": "/static/css/main.2f992b19.chunk.css"
+  },
+  {
+    "revision": "d2089bc7b2b465c26547347f965b646c",
+    "url": "/index.html"
+  }
+];
